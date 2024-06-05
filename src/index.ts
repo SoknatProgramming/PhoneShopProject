e477922b82e94a8462de9d653ac52f958d5b94bb
SoknatProgramming <soknatsann@gmail.com>
2024-06-05 15:46:24 +0700

    add supertest and jasmine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World soknat");
+import express, { Application, Request, Response } from 'express';
+
+const PORT = 3000;
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is starting on port:${PORT}`);
+});
+
+export default app;
